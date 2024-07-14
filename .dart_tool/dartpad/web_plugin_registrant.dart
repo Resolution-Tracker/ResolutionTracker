0d3710a759f0cb9f2f403ce2de042112bc2258f7
ColbyLairdieson <cjlclimber@gmail.com>
2024-07-14 11:17:19 -0400

    fixing things

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,20 +3,6 @@
 // Generated file. Do not edit.
 //
 
-<<<<<<< Updated upstream
-// @dart = 2.13
-// ignore_for_file: type=lint
-
-import 'package:shared_preferences_web/shared_preferences_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-
-void registerPlugins([final Registrar? pluginRegistrar]) {
-  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  SharedPreferencesPlugin.registerWith(registrar);
-  registrar.registerMessageHandler();
-}
-=======
 // ignore_for_file: type=lint
 
 void registerPlugins() {}
->>>>>>> Stashed changes
